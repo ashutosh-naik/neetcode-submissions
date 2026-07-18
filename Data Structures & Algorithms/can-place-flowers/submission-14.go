685850f25ef0bfff509296312cb31c5a0028a2ff
Ashutosh Naik <60225315+ashutosh-naik@users.noreply.github.com>
2026-07-18 22:48:43 +0530

    Add: can-place-flowers - submission-14

---
+++ Data Structures & Algorithms/can-place-flowers/submission-14.go
@@ -0,0 +1,27 @@
+func canPlaceFlowers(flowerbed []int, n int) bool {
+
+	if n == 0 {
+		return true
+	}
+
+	count := 0
+
+	for i := 0; i < len(flowerbed); i++ {
+
+		if flowerbed[i] == 0 &&
+			(i == 0 || flowerbed[i-1] == 0) &&
+			(i == len(flowerbed)-1 || flowerbed[i+1] == 0) {
+
+			flowerbed[i] = 1
+			count++
+
+			if count >= n {
+				return true
+			}
+
+			i++ // Skip the next plot
+		}
+	}
+
+	return false
+}
